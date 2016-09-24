82755b882e9dfa0df49937edebde3c1fb21a07b5
iamAkash <akash@nexthoughts.com>
2016-09-24 11:17:29 +0530

    Create initial level mapping for domains

--- grails-app/domain/com/backend/Issue.groovy
+++ grails-app/domain/com/backend/Issue.groovy
@@ -5,8 +5,21 @@ class Issue implements Serializable {
     String name
     String description
 
+    Date dateCreated
+    Date lastUpdated
+    User createdBy
+    User modifiedBy
+
+    static belongsTo = [
+            project: Project
+    ]
+
     static constraints = {
         name nullable: false, blank: false, matches: /[a-zA-Z0-9_-]+/
         description nullable: true
+        createdBy nullable: true
+        modifiedBy nullable: true
+        dateCreated nullable: true
+        lastUpdated nullable: true
     }
 }
--- grails-app/domain/com/backend/Project.groovy
+++ grails-app/domain/com/backend/Project.groovy
@@ -5,8 +5,23 @@ class Project implements Serializable {
     String name
     String description
 
+    Date dateCreated
+    Date lastUpdated
+    User createdBy
+    User modifiedBy
+
+    static hasMany = [
+            issues    : Issue,
+            labels    : Label,
+            milestones: Milestone
+    ]
+
     static constraints = {
         name nullable: false, blank: false, matches: /[a-zA-Z0-9_-]+/
         description nullable: true
+        createdBy nullable: true
+        modifiedBy nullable: true
+        dateCreated nullable: true
+        lastUpdated nullable: true
     }
 }
