dd4660b4d718603a5b2a6fdde7ebc23102d9a41a
iamAkash <akash@nexthoughts.com>
2016-09-24 16:27:03 +0530

    Commit

--- grails-app/controllers/hackathon/HomeController.groovy
+++ grails-app/controllers/hackathon/HomeController.groovy
@@ -1,19 +1,20 @@
 package hackathon
+
 import grails.plugin.springsecurity.annotation.Secured
 
 @Secured('permitAll')
 class HomeController {
     def index() {
         println "mmmmmmmmmmmmmmmmmmmmm"
-        render view:"home"
+        render view: "home"
     }
 
-    def sendmail(){
-println("sending.........")
+    def sendmail() {
+        println("sending.........")
         sendMail {
             to "anuj@nexthoughts.com"
             subject "Hello John"
-            html g.render(template:"/templates/email")
+            html g.render(template: "/templates/email")
         }
         println("sending success.........")
 
