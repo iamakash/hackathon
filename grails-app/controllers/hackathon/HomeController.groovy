0cb3072519130f3abe25c3c425fbadd7c737189a
Anuj Nigam <anujnigam.friends@gmail.com>
2016-09-24 15:32:55 +0530

    mail updated

--- grails-app/controllers/hackathon/HomeController.groovy
+++ grails-app/controllers/hackathon/HomeController.groovy
@@ -4,18 +4,14 @@ import grails.plugin.springsecurity.annotation.Secured
 @Secured('permitAll')
 class HomeController {
     def index() {
-        println "mmmmmmmmmmmmmmmmmmmmm"
         render view:"home"
     }
 
     def sendmail(){
-println("sending.........")
         sendMail {
             to "anuj@nexthoughts.com"
             subject "Hello John"
             html g.render(template:"/templates/email")
         }
-        println("sending success.........")
-
     }
 }
